2337367f4e9657ceba92e16da61922d8d66449e8
David Rosas <59243448+KaruRose@users.noreply.github.com>
2023-05-24 01:43:53 +0000

    instalado dotenv y modificado supabase

--- src/config/supabse.js
+++ src/config/supabse.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ltizaydbqwhxdmairroq.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0aXpheWRicXdoeGRtYWlycm9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQzNDkyMDYsImV4cCI6MTk5OTkyNTIwNn0.l4XQ3jX8FmnZh_1Uqujob-n4VRu4QMWEfssiW4mVWpo'
+const supabaseKey = process.env.API_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
